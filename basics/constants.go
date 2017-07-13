64cfb5c43f4c6c40f4e6f37a510b637de088526b
dextermoran <dextermoran@gmail.com>
2017-07-12 21:28:23 -0500

    constants

---
+++ basics/constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const p string = "Death and Taxes"
+
+const (
+  Pi = 3.14
+  Pie = "cherry"
+
+  P = iota // 1
+)
+
+func main() {
+  const q = 42
+
+  fmt.Println("p - ", p)
+  fmt.Println("q - ", q)
+
+  fmt.Println(PI)
+  fmt.Println(Pie)
+}
