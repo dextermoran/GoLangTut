4eb4af53023e5a2220e144ecb4d44d0e99896475
dextermoran <dextermoran@gmail.com>
2017-07-18 17:26:40 -0500

    functions

--- basics/functions/funcBasic.go
+++ basics/functions/funcBasic.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println(funko("eleven"))
 
+	n := average(22, 33, 44, 100)
+	fmt.Println(n)
+}
+
+func funko(name string) string {
+	return fmt.Sprint("upside down ", name)
+}
+
+func average(x ...float64) float64 {
+	fmt.Println(x)
+	fmt.Printf("%T", x)
+	var total float64
+
+	for _, v := range x {
+		total += v
+	}
+
+	return total / float64(len(x))
 }
